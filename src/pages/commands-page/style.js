d52b3958c95c09326307567e4c02c10d743ff5e4
iLaD08 <imadeddinelakhdar08@gmail.com>
2020-12-31 21:32:55 +0100

    added a custom scroll bar and edited the position of CommandsDivContainer

--- src/pages/commands-page/style.js
+++ src/pages/commands-page/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const CommandsDivContainer = styled.div`
     position: relative;
-    top: 50px;
+    top: 60px;
     display: flex;
     justify-content: center;
 `;
